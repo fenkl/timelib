9679cd4a6be64c78d98173331d5a9109a4bfa666
fenkl <franciscofenkl@gmail.com>
2022-05-10 10:19:17 +0200

    added day of the year function

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /**
- *
+ * Timelib
  *
  *
  **/
@@ -8,14 +8,73 @@
 #include <stdlib.h>
 
 /**
- *
- *
+ * Der Funktion werden Tag, Monat und Jahr übergeben.
+ * Rückgabewert ist der Tag des jahres.
  **/
 int day_of_the_year(int day, int month, int year)
 {
+    int jahr, monat, tag, rest, schalt;
+    printf("Dies ist ein Rechner f�r die Zahl des Tages!\n");
+    printf("Bitte Jahr angeben:\n");
+    scanf("%i",&jahr);
+
+
+    int anzahl[12]= {31,28,31,30,31,30,31,31,30,31,30,31};
+    rest = jahr % 4;
+    if (rest == 0)
+    {
+       rest = jahr % 100;
+       if (rest == 0)
+       {
+          rest = jahr % 400;
+          if(rest != 0)
+          {
+             schalt = 0;
+          }
+          else
+          {
+             schalt = 1;
+          }
+       }
+       else
+       {
+          schalt = 1;
+       }
+    }
+    else
+    {
+       schalt=0;
+    }
+
+    if (schalt == 1)
+    {
+       anzahl[1] =29;
+    }
+
+    do{
+      printf("Bitte Monat angeben:\n");
+      scanf("%i",&monat);}
+    while(monat<=0 || monat>12 );
+
+    do{
+      printf("Bitte Tag angeben:\n");
+      scanf("%i",&tag);}
+    while(tag> anzahl[monat-1] || tag<=0);
+
+    int ergebnis = 0;
+
+    for (int i = 0; i< monat-1; i++)
+    {
+    ergebnis+=anzahl[i];
+    }
+    ergebnis+=tag;
+
+    printf("\n Der angegebene Tag ist der %i Tag des Jahres %i\n", ergebnis, jahr);
+
 
     return 0;
 }
+
 
 int main()
 {
